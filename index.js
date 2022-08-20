1837524cabb57a297fe61d096c2c73079e75c80b
user <gregorlitvinov@gmail.com>
2022-08-20 15:57:51 +0300

    locStorg-test

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 window.onload = function () {
+
+let todoList = []
+  if(localStorage.getItem('todo') != undefined) {
+  todoList = JSON.parse(localStorage.getItem('todo'))
+  out()
+  }
+
   document.getElementById('add').onclick = function() {
     let val = document.getElementById('in').value
+    let temp = {}
+    temp.todo = val
+    temp.check = false
+    let i = todoList.length
+    todoList[i] = temp
+    console.log(todoList)
+    out()
+    localStorage.setItem('todo', JSON.stringify(todoList))
   }
+   function out () {
+      let out = '';
+    //   for (let key in todoList) {
+    //     if(todoList[key].check == true) {
+    //       out += '<input type="checkbox" checked/>'
+    //   } else {
+    //      out += '<input type="checkbox" />'
+    //   }
+    // }
+      for (let key in todoList) {
+        console.log(todoList[key])
+        out += todoList[key].todo + '<br>'
+      }
+    document.getElementById('out').innerHTML = out
+
+   }
 }
