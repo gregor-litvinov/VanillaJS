615e7906b05ee8fbe5e98faa680db7747c43a39a
user <gregorlitvinov@gmail.com>
2022-08-24 18:46:08 +0300

    gameX0

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 window.onload = function () {
+  for (let i = 0; i < 9; i++)
+  document.getElementById('game').innerHTML+='<div class="box"></div>'
 
-  document.querySelector('.tabs-header').addEventListener('click', fTabs)
+  let hod = 0;
 
-    function fTabs(event) {
-      // console.log(event)
-      // console.log(event.target.getAttribute('data-tab'))
-      // console.log(event.target.className == 'tab-h')
-      if (event.target.className == 'tabs-h') {
-        let dataTab = event.target.getAttribute('data-tab')
-        let tabBody = document.getElementsByClassName('tab-b')
-        for (let i = 0; i < tabBody.length; i++) {
-          if (dataTab == i) {
-            tabBody[i].style.display = 'block'
-          } else {
-             tabBody[i].style.display = 'none'
-          }
-        }
-        // console.log(dataTab)
+  document.getElementById('game').onclick = function(event) {
+    console.log(event)
+    if (event.target.className == "box") {
+      if(hod % 2 == 0) {
+        event.target.innerHTML = 'x'
+      } else {
+        event.target.innerHTML = '0'
       }
-
+      hod++
+      checkWiner()
     }
+  }
+  function checkWiner() {
+    let allBlock = document.getElementsByClassName('box');
+    if (allBlock[0].innerHTML == 'x' && allBlock[1].innerHTML == 'x' && allBlock[2].innerHTML == 'x') alert('Winner')
+    if (allBlock[3].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[5].innerHTML == 'x') alert('Winner')
+    if (allBlock[6].innerHTML == 'x' && allBlock[7].innerHTML == 'x' && allBlock[8].innerHTML == 'x') alert('Winner')
+    if (allBlock[0].innerHTML == 'x' && allBlock[3].innerHTML == 'x' && allBlock[6].innerHTML == 'x') alert('Winner')
+    if (allBlock[1].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[7].innerHTML == 'x') alert('Winner')
+    if (allBlock[2].innerHTML == 'x' && allBlock[5].innerHTML == 'x' && allBlock[8].innerHTML == 'x') alert('Winner')
+    if (allBlock[0].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[8].innerHTML == 'x') alert('Winner')
+    if (allBlock[0].innerHTML == 'x' && allBlock[1].innerHTML == 'x' && allBlock[2].innerHTML == 'x') alert('Winner')
+    if (allBlock[2].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[6].innerHTML == 'x') alert('Winner')
+    //zero
+    if (allBlock[0].innerHTML == '0' && allBlock[1].innerHTML == '0' && allBlock[2].innerHTML == '0') alert('Winner')
+    if (allBlock[3].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[5].innerHTML == '0') alert('Winner')
+    if (allBlock[6].innerHTML == '0' && allBlock[7].innerHTML == '0' && allBlock[8].innerHTML == '0') alert('Winner')
+    if (allBlock[0].innerHTML == '0' && allBlock[3].innerHTML == '0' && allBlock[6].innerHTML == '0') alert('Winner')
+    if (allBlock[1].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[7].innerHTML == '0') alert('Winner')
+    if (allBlock[2].innerHTML == '0' && allBlock[5].innerHTML == '0' && allBlock[8].innerHTML == '0') alert('Winner')
+    if (allBlock[0].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[8].innerHTML == '0') alert('Winner')
+    if (allBlock[0].innerHTML == '0' && allBlock[1].innerHTML == '0' && allBlock[2].innerHTML == '0') alert('Winner')
+    if (allBlock[2].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[6].innerHTML == '0') alert('Winner')
+  }
 }
