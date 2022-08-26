1e6abb9f86cb9214786c753a57cd06ea10088ca4
user <gregorlitvinov@gmail.com>
2022-08-26 18:04:13 +0300

    gameStart

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
 window.onload = function () {
-  for (let i = 0; i < 9; i++)
-  document.getElementById('game').innerHTML+='<div class="box"></div>'
+let number = Math.floor(10*Math.random()) + 1;
+console.log(number)
+let count = 2;
 
-  let hod = 0;
+document.getElementById('check').onclick = function() {
 
-  document.getElementById('game').onclick = function(event) {
-    console.log(event)
-    if (event.target.className == "box") {
-      if(hod % 2 == 0) {
-        event.target.innerHTML = 'x'
-      } else {
-        event.target.innerHTML = '0'
-      }
-      hod++
-      checkWiner()
+if (count > 0) {
+  let userNam = document.getElementById('mynum').value;
+  console.log(userNam)
+  userNam = parseInt(userNam)
+
+  let out = document.getElementById('out')
+
+    if (userNam == number) {
+    out.innerHTML = 'winner!'
     }
+    else if (userNam > number) {
+    out.innerHTML = 'cold!'
+    }
+    else if (userNam < number) {
+    out.innerHTML = 'hot!'
+    }
+    document.getElementById('count').innerHTML = 'Attention! you can try eat' + count;
+     count = count - 1;
   }
-  function checkWiner() {
-    let allBlock = document.getElementsByClassName('box');
-    if (allBlock[0].innerHTML == 'x' && allBlock[1].innerHTML == 'x' && allBlock[2].innerHTML == 'x') alert('Winner')
-    if (allBlock[3].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[5].innerHTML == 'x') alert('Winner')
-    if (allBlock[6].innerHTML == 'x' && allBlock[7].innerHTML == 'x' && allBlock[8].innerHTML == 'x') alert('Winner')
-    if (allBlock[0].innerHTML == 'x' && allBlock[3].innerHTML == 'x' && allBlock[6].innerHTML == 'x') alert('Winner')
-    if (allBlock[1].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[7].innerHTML == 'x') alert('Winner')
-    if (allBlock[2].innerHTML == 'x' && allBlock[5].innerHTML == 'x' && allBlock[8].innerHTML == 'x') alert('Winner')
-    if (allBlock[0].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[8].innerHTML == 'x') alert('Winner')
-    if (allBlock[0].innerHTML == 'x' && allBlock[1].innerHTML == 'x' && allBlock[2].innerHTML == 'x') alert('Winner')
-    if (allBlock[2].innerHTML == 'x' && allBlock[4].innerHTML == 'x' && allBlock[6].innerHTML == 'x') alert('Winner')
-    //zero
-    if (allBlock[0].innerHTML == '0' && allBlock[1].innerHTML == '0' && allBlock[2].innerHTML == '0') alert('Winner')
-    if (allBlock[3].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[5].innerHTML == '0') alert('Winner')
-    if (allBlock[6].innerHTML == '0' && allBlock[7].innerHTML == '0' && allBlock[8].innerHTML == '0') alert('Winner')
-    if (allBlock[0].innerHTML == '0' && allBlock[3].innerHTML == '0' && allBlock[6].innerHTML == '0') alert('Winner')
-    if (allBlock[1].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[7].innerHTML == '0') alert('Winner')
-    if (allBlock[2].innerHTML == '0' && allBlock[5].innerHTML == '0' && allBlock[8].innerHTML == '0') alert('Winner')
-    if (allBlock[0].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[8].innerHTML == '0') alert('Winner')
-    if (allBlock[0].innerHTML == '0' && allBlock[1].innerHTML == '0' && allBlock[2].innerHTML == '0') alert('Winner')
-    if (allBlock[2].innerHTML == '0' && allBlock[4].innerHTML == '0' && allBlock[6].innerHTML == '0') alert('Winner')
-  }
+else {
+      alert('you lost')
+      window.location.reload()
+}
+}
 }
