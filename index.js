f5311bfd286dd168e9b28d3abec8cd1d685ca3f9
user <gregorlitvinov@gmail.com>
2022-08-27 23:22:04 +0300

    inerval

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 window.onload = function () {
 
-  let block = document.getElementById('one')
+  let sdvig = 0;
 
-  // block.onclick = function() {
-  //   this.style.background = 'green'
-  //   this.onclick = null
-  // }
-  // block.ondblclick = function() {
-  //   this.style.background = 'red'
-  // }
-    block.oncontextmenu = function() {
-    this.style.background = 'black'
-    return false
+  let test = document.getElementById('test')
+  move()
+
+  function move() {
+    test.style.marginLeft = sdvig + 'px'
+    sdvig = sdvig + 10;
+    setTimeout(move, 50)
   }
-  // block.onmouseenter = function() {
-  //   console.log('...in!')
-  //   this.style.background = 'gold'
-  // }
-  // block.onmouseleave = function() {
-  //   this.style.background = 'blue'
-  // }
-    //   let a = 0;
-    //   block.onmousemove = function() {
-    //   a++
-    //   this.style.width = 100 + a +'px'
-    // }
-    block.onmousedown = function(event) {
-      this.style.background = 'cyan'
-      console.log(event.button)
 
-    }
+  // let timer = setInterval(move, 50)
+
+  document.getElementById('stop').onclick = function() {
+    // clearInterval(timer)
+  }
+  // setTimeout(move, 5000)
 }
