b6c8c1b533ff0a17d2e14343be9e62fdb77cd6b4
user <gregorlitvinov@gmail.com>
2022-08-28 05:59:19 +0300

    timers

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 window.onload = function () {
 
   let sdvig = 0;
+  let timer;
 
   let test = document.getElementById('test')
-  move()
+  // move()
 
   function move() {
     test.style.marginLeft = sdvig + 'px'
     sdvig = sdvig + 10;
-    setTimeout(move, 50)
+    timer = setTimeout(move, 50)
   }
 
   // let timer = setInterval(move, 50)
 
   document.getElementById('stop').onclick = function() {
     // clearInterval(timer)
+    clearTimeout(timer)
+  }
+  let a = 10
+  ort()
+  function ort() {
+    document.getElementById('out').innerHTML = a;
+    a--;
+    if (a < 0) {
+      clearTimeout(timer)
+    } else {
+      timer = setTimeout(ort, 1000)
+    }
   }
   // setTimeout(move, 5000)
 }
