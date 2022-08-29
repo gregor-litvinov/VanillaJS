b9b3359d0ba2ac10636c2f3af74a4660809b20a9
user <gregorlitvinov@gmail.com>
2022-08-29 17:42:28 +0300

    mapsAdd

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 window.onload = function () {
 
-  let left = document.querySelector('.left')
-  let right =  document.querySelector('.right')
-
-  function equalHeight() {
-
-      left.style.height = 'auto';
-      right.style.height = 'auto';
-      let leftH = left.offsetHeight
-      let rightH = right.offsetHeight
-        console.log(left)
-
-      let max = Math.max(leftH, rightH)
-       left.style.height = max + 'px';
-       right.style.height = max + 'px'
-  }
-    equalHeight()
-
-    window.onresize = equalHeight
+  
 }
